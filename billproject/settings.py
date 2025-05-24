6fc217f126c9c60f0297d8aceb63f76ca525008f
Irfanme007 <meirfan1997@gmail.com>
2025-05-24 15:33:27 +0530

    'listingsapp_removed'

--- billproject/settings.py
+++ billproject/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'stockapp',
     'billingapp',
-    'listingsapp',
     'widget_tweaks',
 ]
 
@@ -82,12 +81,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT', default='5432'),
     }
 }
+
 
 
 # Password validation
